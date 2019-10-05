eafbb831f738a4ada21f5982c1dd978856a9fe1f
Brandon Shepard <brandonshepard@vip3r.wi.rr.com>
2019-10-05 16:30:35 -0500

    added some life to the main pic, personalized dialogs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,37 @@
-document.querySelector('img').onclick = function(){
-    alert("Hai, fren!");
+let mainPic = document.getElementById('mainPic');
+
+let resetNameButton = document.getElementById('reset-name-button');
+
+resetNameButton.onclick = function(){
+    setUserName();
 }
+
+mainPic.onclick = function(){
+    let name = localStorage.getItem('name');
+    console.log('name: ', name);
+    if(!name || name === 'null' || name === ''){
+        setUserName();
+    }else{
+       alert('Hai, ' + name + '! ♥️ ♥️ ♥️'); 
+    }
+}
+
+setInterval(mainPicSwap,1000);
+
+function mainPicSwap () {
+    let mySrc = mainPic.getAttribute('src');
+    if(mySrc === 'images/pixelSmiles.png'){
+        mainPic.setAttribute('src', 'images/pixelatedComputer.png');
+    }else if(mySrc === 'images/pixelatedComputer.png'){
+        mainPic.setAttribute('src', 'images/pixelSmiles.png');
+    }
+}
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName && myName === null){
+        setUserName();
+    }else{
+       localStorage.setItem('name', myName); 
+    }
+  }
